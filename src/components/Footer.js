a1025d4d225bdfc851260dc29e703a0d53561d12
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-02-26 01:05:33 +0100

    full home page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <p>Copyright by Alicja Sumińska 2022</p>
+    </div>
+  );
+};
+
+export default Footer;
