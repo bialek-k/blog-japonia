e9b48161d7a1b3d7c2cd11e3c01ba2c7aaee8b94
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-03-01 10:20:27 +0100

    article content

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Article_content from './Article_content'
 import Article_header from './Article_header'
 
 const Article = () => {
   return (
     <div>
       <Article_header/>
+      <Article_content/>
     </div>
   )
 }
---
+++ src/components/Article_content.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Article_content.module.css'
+
+const Article_content = () => {
+  return (
+    <div className={styles.Article_content}>
+      <div className={styles.container}>
+        <div className={styles.ksiusiu}>
+          <p>Ksiusiu</p>
+        </div>
+        <p>Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Obwód kaldery wynosi około 128 km. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu.  Ma wysokość 1 592 m n.p.m. </p>
+
+        <p>Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Obwód kaldery wynosi około 128 km. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu.  Ma wysokość 1 592 m n.p.m.  Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Obwód kaldery wynosi około 128 km. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu.  Ma wysokość 1 592 m n.p.m. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. 
+        </p>
+
+        <div className={styles.img}></div>
+
+        <div className={styles.quote_container}>
+          <p className={styles.quote}>“Wchodząc późną zimą do pawilonu herbacianego możemy ujrzeć wiotką gałązkę dzikiej wiśni zestawioną z pączkiem kamelii - to echo odchodzącej zimy i zapowiedź wiosny.”
+            <span>Kakuzo Okakura - Księga Herbaty</span>
+          </p>
+        </div>
+
+        <div className={styles.wrapper}>
+          <div className={styles.img2}></div>
+          <div className={styles.wrapper_in}>
+            <h2>Życie wokół wulkanu </h2>
+            <p>Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Obwód kaldery wynosi około 128 km. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m.  Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Obwód kaldery wynosi około 128 km. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie.Znajduje się na wyspie Kiusiu. Ma wysokość 1 592 m n.p.m. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. Ma wysokość 1 592 m n.p.m. Góra Aso to największy aktywny masyw wulkaniczny w Japonii i jeden z największych na świecie. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Article_content
