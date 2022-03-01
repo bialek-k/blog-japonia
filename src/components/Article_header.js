4afb428cf8a81a4531e3f717916a5cd6200e65c8
damianhaq <40294540+damianhaq@users.noreply.github.com>
2022-03-01 09:14:18 +0100

    classname update

--- src/components/Article_header.js
+++ src/components/Article_header.js
@@ -3,7 +3,7 @@ import styles from './Article_header.module.css'
 
 const Article_header = () => {
   return (
-    <div className={styles.header}>
+    <div className={styles.article_header}>
       <h1>GÓRA ASO</h1>
       <h2>najwyższy wulkan w Japonii</h2>
       <div className={styles.read_time}>
