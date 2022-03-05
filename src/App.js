bc0809233aea85aa2dd04c7c92be0ce6b110bd92
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-03-05 01:31:33 +0100

    contact-page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-
 import Nav from "./components/Nav";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Article from "./components/Article";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Nav/>
+        <Nav />
         <Routes>
-          <Route path="/blog-japonia" element={<HomePage/>}/>
-          <Route path="/Article" element={<Article/>}/>
-          
+          <Route path="/blog-japonia" element={<HomePage />} />
+          <Route path="/Article" element={<Article />} />
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
